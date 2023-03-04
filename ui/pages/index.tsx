5de0df2aed11c4e7ea74fd0ed83559581845247b
HeeJun Moon <aglide100@gmail.com>
2023-03-04 19:28:31 +0900

    add widget

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,107 +1,129 @@
+/* eslint-disable @next/next/no-script-component-in-head */
+/* eslint-disable @next/next/no-sync-scripts */
 import React, { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import * as axios from "axios";
 import {
-  CardContainer,
-  CardContainerProps,
-  CardContainerDataType,
+    CardContainer,
+    CardContainerProps,
+    CardContainerDataType,
 } from "../components/CardContainer/CardContainer";
 import { motion, AnimateSharedLayout } from "framer-motion";
 import Switch from "react-switch";
+import Script from "next/script";
 
 const Home: NextPage = () => {
-  let cardList;
-  const [data, setData] = useState<any>();
-  const [domain, setDomain] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isClick, setIsClick] = useState<boolean>(false);
-  //   const [isHover, setIsHover] = useState<boolean>(false);
-  function getEnv() {
-    const axiosObj = axios.default;
-    axiosObj.get("/env/").then((res) => {
-      setData(res.data.env);
-      setDomain(res.data.Domain1);
-      console.log("Domain :", domain);
-      setIsLoading(true);
-    });
-  }
-
-  // function onHover(e: any) {
-  //     e.preventDefault();
-  //     setIsHover(!isHover);
-  // }
-
-  useEffect(() => {
-    if (!isLoading) {
-      getEnv();
+    let cardList;
+    const [data, setData] = useState<any>();
+    const [domain, setDomain] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isClick, setIsClick] = useState<boolean>(false);
+    //   const [isHover, setIsHover] = useState<boolean>(false);
+    function getEnv() {
+        const axiosObj = axios.default;
+        axiosObj.get("/env/").then((res) => {
+            setData(res.data.env);
+            setDomain(res.data.Domain1);
+            console.log("Domain :", domain);
+            setIsLoading(true);
+        });
     }
-  });
 
-  if (isLoading && data != undefined) {
-    cardList = Object.keys(data).map((key, i) => {
-      let serviceName = key as CardContainerDataType;
+    // function onHover(e: any) {
+    //     e.preventDefault();
+    //     setIsHover(!isHover);
+    // }
 
-      if (data[key] == true) {
-        return (
-          <CardContainer
-            key={"list_" + i}
-            svcName={serviceName}
-            domain={domain}
-            isClick={isClick}
-          ></CardContainer>
-        );
-      }
+    useEffect(() => {
+        if (!isLoading) {
+            getEnv();
+        }
     });
-  } else {
-    cardList = <>Loading...</>;
-  }
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Hello</title>
-        <meta name="description" content="" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
+    let location;
+    let windowTest;
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Hello</h1>
-        <div style={{ marginTop: "5px" }}>
-          <Switch
-            onChange={(e) => {
-              setIsClick(!isClick);
-            }}
-            checked={isClick}
-          />
-        </div>
+    if (process.browser) {
+        location = document.location;
+        windowTest = window.location.href;
 
-        {/* <p className={styles.description}>
+        let script = document.createElement("script");
+        script.src = "/live2d-widget/autoload.js";
+
+        script.async = true;
+        if (isClick && isLoading) {
+            document.body.appendChild(script);
+        } else {
+            if (document.body.hasAttribute("script")) {
+                document.body.removeChild(script);
+            }
+        }
+    }
+
+    if (isLoading && data != undefined) {
+        cardList = Object.keys(data).map((key, i) => {
+            let serviceName = key as CardContainerDataType;
+
+            if (data[key] == true) {
+                return (
+                    <CardContainer
+                        key={"list_" + i}
+                        svcName={serviceName}
+                        domain={domain}
+                        isClick={isClick}
+                    ></CardContainer>
+                );
+            }
+        });
+    } else {
+        cardList = <>Loading...</>;
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Hello</title>
+                <meta name="description" content="" />;
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Hello</h1>
+                <div style={{ marginTop: "5px" }}>
+                    <Switch
+                        onChange={(e) => {
+                            setIsClick(!isClick);
+                        }}
+                        checked={isClick}
+                    />
+                </div>
+
+                {/* <p className={styles.description}>
                     <code className={styles.code}>pages/index.tsx</code>
                 </p> */}
 
-        <motion.div className={styles.grid}>
-          <AnimateSharedLayout>
-            <motion.ul
-              layout
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                padding: "10rem",
-                justifyContent: "space-around",
-                flexWrap: "wrap",
-              }}
-            >
-              {cardList}
-            </motion.ul>
-          </AnimateSharedLayout>
-        </motion.div>
-      </main>
+                <motion.div className={styles.grid}>
+                    <AnimateSharedLayout>
+                        <motion.ul
+                            layout
+                            style={{
+                                display: "flex",
+                                flexDirection: "row",
+                                padding: "10rem",
+                                justifyContent: "space-around",
+                                flexWrap: "wrap",
+                            }}
+                        >
+                            {cardList}
+                        </motion.ul>
+                    </AnimateSharedLayout>
+                </motion.div>
+            </main>
 
-      <footer className={styles.footer}></footer>
-    </div>
-  );
+            <footer className={styles.footer}></footer>
+        </div>
+    );
 };
 
 export default Home;
